c298d82837e3b14b0fe557be3a49ac4c8a44fc89
Sebastianville <sebastian.villegas2623@gmail.com>
2025-01-29 15:22:22 -0500

    second part completed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //! part 1//
 //select anad cache the main element
-const mainEl = document.querySelector('main')
+const mainEl = document.querySelector('main');
 
 //set the background color to the value of main bg CSS custom property 
-mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.style.backgroundColor = 'var(--main-bg)';
 
 //set the content mainEl to h1
-mainEl.innerHTML = '<h1>DOM Manipulation</h1>'
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 
 // Add the class "flex-ctr" to mainEl
 mainEl.classList.add('flex-ctr');
@@ -14,5 +14,14 @@ mainEl.classList.add('flex-ctr');
 
 //! part 2//
 //select and chace the nav elemen 
-const topMenuEl = document.getElementById('top-menu')
-console.log(topMenuEl)
+const topMenuEl = document.getElementById('top-menu');
+// console.log(topMenuEl)
+
+//set the height 
+topMenuEl.style.height = '100%';
+
+//set the background color to the value in css custom property 
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+//Add the class flex around 
+topMenuEl.classList.add('flex-around');
