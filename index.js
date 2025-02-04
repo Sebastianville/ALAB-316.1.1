429f507d34211f4fa84b4b56c2a00aa91918611b
Sebastianville <sebastian.villegas2623@gmail.com>
2025-02-04 18:51:39 -0500

    part 2 section 4 completed

--- index.js
+++ index.js
@@ -29,15 +29,26 @@ topMenuEl.classList.add('flex-around');
 
 //! part 3//
 const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
-  menuLinks.forEach(link => {
-    // Create an a element
-    const linkEl = document.createElement('a');
+menuLinks.forEach(link => {
+  // Create an a element
+  const linkEl = document.createElement('a');
 
   // Set the href attribute
   linkEl.setAttribute('href', link.href);
@@ -47,7 +58,11 @@ const menuLinks = [
   
   // Append the a element to topMenuEl
   topMenuEl.appendChild(linkEl);
+
+  
 });
+
+
 
 //! part TWO 3.1//
 const subMenuEl = document.getElementById('sub-menu');
@@ -66,4 +81,23 @@ subMenuEl.style.position = "absolute";
 
 //set the top property to the value of 0
 subMenuEl.style.top = "0px"; 
+
+//! part 2.4//
+const topMenuLinks = document.querySelectorAll('a');
+console.log(topMenuLinks)
+
+topMenuEl.addEventListener('click', function(e) {
+  e.preventDefault()
+  if (!e.target.matches('a')) {
+    return 
+  } console.log(e.target.textContent);
+  
+//remove the "active" class from all links
+  topMenuLinks.forEach(link => {
+    console.log(`Removing active from: ${link.textContent}`);
+    link.classList.remove('active');
+  });
+  // Toggle "active" class on the clicked link
+  e.target.classList.toggle('active');
+});
 
